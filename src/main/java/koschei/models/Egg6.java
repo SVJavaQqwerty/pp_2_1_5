32df45266425015f07a4a9a04c60875d13c2df9b
пк <mr.setevik@rambler.ru>
2023-03-23 19:55:32 +0300

    Исправлено

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -3,9 +3,8 @@ package koschei.models;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
 public class Egg6 {
-    @Autowired
+
     Needle7 needle7;
 
     public Egg6(Needle7 needle7) {this.needle7 = needle7;}
--- src/main/java/koschei/models/Island2.java
+++ src/main/java/koschei/models/Island2.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Component;
 public class Island2 {
 
     private Wood3 wood;
-    @Autowired
     public Island2(Wood3 wood) {
         this.wood = wood;
     }
